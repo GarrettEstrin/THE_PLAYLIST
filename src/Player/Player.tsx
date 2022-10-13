6cc4f87524b9a9029f001e57d3dd4c9ee102a484
Garrett <garrett.estrin@gmail.com>
2022-10-13 12:59:57 -0700

    Added player buttons

--- src/Player/Player.tsx
+++ src/Player/Player.tsx
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay, faPause, faBackwardStep, faForwardStep } from '@fortawesome/free-solid-svg-icons'
+
 import { useAppContext } from '../Utilities/AppContext';
 import './player.css';
 
 function Player() {
-  const { library, currentSong } = useAppContext();
-  console.log(library, currentSong);
+  const [song, setSong] = useState<HTMLAudioElement | null>(null);
+  const [playing, setPlaying] = useState(false);
+  const { currentSong } = useAppContext();
+
+  useEffect(() => {
+    setSong(new Audio('/audio/' + currentSong + '.mp3'))
+  }, [currentSong]);
+
+  const play = () => {
+    if (song !== null) {
+      song.play();
+      setPlaying(true);
+    }
+  };
+
+  const back = () => {
+    alert("You don't want to go back. Let's let this one ride out.");
+  };
+
+  const next = () => {
+    alert("You don't want to skip this one. It's a banger!");
+  };
+
+  const pause = () => {
+    if (song !== null) {
+      song.pause();
+      setPlaying(false);
+    }
+  };
   return (
     <div className="player">
-      This is the player
+      <div onClick={back}><FontAwesomeIcon icon={faBackwardStep} /></div>
+      {playing ?
+        <div onClick={pause}><FontAwesomeIcon icon={faPause} /></div> :
+        <div onClick={play}><FontAwesomeIcon icon={faPlay} /></div>
+      }
+      <div onClick={next}><FontAwesomeIcon icon={faForwardStep} /></div>
     </div>
   )
 };
--- src/Utilities/AppContext.tsx
+++ src/Utilities/AppContext.tsx
@@ -10,6 +10,8 @@ interface DefaultContextType {
   library: {},
   currentSong: string,
   setCurrentSong: React.Dispatch<SetStateAction<Song>>,
+  supportsDB: boolean,
+  // dbInitialized: boolean,
 }
 
 const defaultContext: DefaultContextType = {
@@ -18,6 +20,8 @@ const defaultContext: DefaultContextType = {
   library,
   currentSong: Song.Butterfly,
   setCurrentSong: () => Song,
+  supportsDB: false,
+  // dbInitialized: false,
 };
 
 export const AppContext = createContext(defaultContext);
@@ -29,13 +33,25 @@ export const useAppContext = () => {
 export const AppContextProvider = (props: { children: ReactElement }) => {
   const [currentView, setCurrentView] = useState(View.Home);
   const [currentSong, setCurrentSong] = useState(Song.Butterfly);
+  // const [dbInitialized, setDBInitialized] = useState(false);
+
+  const supportsDB = !!window.indexedDB;
+
+  // const initializeDB = () => {
+  //   if (supportsDB) {
+  //     const request = window.indexedDB.open("library_db", 3);
+  //   }
+  // }
+
   return (
     <AppContext.Provider value={{
       currentView,
       setCurrentView,
       library,
       currentSong,
-      setCurrentSong
+      setCurrentSong,
+      supportsDB,
+      // dbInitialized,
     }}>
       {props.children}
     </AppContext.Provider>
