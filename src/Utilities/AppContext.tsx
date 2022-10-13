cb508089e891cdc0d9ffd0bccb74a0bb665358c0
Garrett <garrett.estrin@gmail.com>
2022-10-13 11:16:29 -0700

    Moved library, added current song and library to context.

--- src/Utilities/AppContext.tsx
+++ src/Utilities/AppContext.tsx
@@ -1,15 +1,23 @@
 import React, { createContext, ReactElement, SetStateAction, useContext, useState } from 'react';
 
 import View from '../Types/views';
+import Song from '../Types/songs';
+import library from './library.json';
 
 interface DefaultContextType {
   currentView: string,
-  setCurrentView: React.Dispatch<SetStateAction<View>>
+  setCurrentView: React.Dispatch<SetStateAction<View>>,
+  library: {},
+  currentSong: string,
+  setCurrentSong: React.Dispatch<SetStateAction<Song>>,
 }
 
 const defaultContext: DefaultContextType = {
   currentView: View.Home,
-  setCurrentView: () => View
+  setCurrentView: () => View,
+  library,
+  currentSong: Song.Butterfly,
+  setCurrentSong: () => Song,
 };
 
 export const AppContext = createContext(defaultContext);
@@ -20,10 +28,14 @@ export const useAppContext = () => {
 
 export const AppContextProvider = (props: { children: ReactElement }) => {
   const [currentView, setCurrentView] = useState(View.Home);
+  const [currentSong, setCurrentSong] = useState(Song.Butterfly);
   return (
     <AppContext.Provider value={{
       currentView,
-      setCurrentView
+      setCurrentView,
+      library,
+      currentSong,
+      setCurrentSong
     }}>
       {props.children}
     </AppContext.Provider>
