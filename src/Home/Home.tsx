f5ea3d8e77347b947436f14105e5eb53f9b6083c
Garrett <garrett.estrin@gmail.com>
2022-10-24 20:28:31 -0700

    Start of home rows

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { useAppContext } from '../Utilities/AppContext';
 import './home.css';
 
 function Home() {
+  const { currentSong } = useAppContext();
+  const generateRow = (type: string) => {
+    const title = type === 'time' ? 'Good Evening' : type;
+    return (
+      <div className="row">
+        <p className="row__title">{title}</p>
+        <div className="row__cont">
+          <div className="row__item-cont">
+            <img className="row__artwork" src={`/album_covers/${currentSong.album_art}`} alt="album art"/>
+            <p className="row__item-title">{currentSong.title}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
   return (
     <div className='view'>
-      This is the home view
+      {generateRow('time')}
     </div>
   )
 };
